58d704f3c7767668c5051cec0e65ed04f156ceb1
Frederick C. Lee <frederick.c.lee@gmail.com>
2016-03-29 12:06:09 -0700

    Creation of Project.

---
+++ ObjC-Context/ObjC-Context/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjC-Context
+//
+//  Created by Frederick C. Lee on 3/29/16.
+//  Copyright © 2016 Amourine Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
